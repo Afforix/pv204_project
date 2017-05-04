4405b88177708e13eb1c696753ce85f6b2354a4a
Adam <janovsky.vm@gmail.com>
2017-05-04 21:03:29 +0200

    Encryption of pw added

--- KeePassApplet.java
+++ KeePassApplet.java
@@ -1,10 +1,12 @@
-/*
-AID: 0113158A9A
- */
+
 package applets;
 
 // IMPORTED PACKAGES
 import javacard.framework.*;
+import javacard.security.AESKey;
+import javacard.security.KeyBuilder;
+import javacard.security.RandomData;
+import javacardx.crypto.Cipher;
 
 
 /**
@@ -12,34 +14,61 @@ import javacard.framework.*;
  */
 public class KeePassApplet extends javacard.framework.Applet {
 
-    // ERROR CODES
+    // Error codes
     final static short SW_BAD_PIN = (short) 0x6900;
     final static short SW_NOT_AUTHENTICATED = (short) 0x6714;
 
-    // MAIN INSTRUCTION CLASS
+    // Main instruction class
     final static byte CLA_KEEPASSAPLET = (byte) 0x80;
 
-    // INSTRUCTIONS
+    // Instructions
     final static byte INS_VERIFYPIN = (byte) 0x00;
     final static byte INS_SENDPASSWD = (byte) 0x01;
     final static byte INS_SETPIN = (byte) 0x02;
     final static byte INS_SETPASSWD = (byte) 0x03;
 
+    // Variables
     private OwnerPIN clientPIN = null;
     private byte password[] = null;
     private short passwordLength = 0;
+    private AESKey m_aesKey = null; // used to encrypt stored password
+    private RandomData m_secureRandom = null; // used to generate the key to store password
+    private Cipher m_encryptCipher = null; // for encrypting and decrypting key
+    private Cipher m_decryptCipher = null;
+    private byte m_ramArray[] = null;
 
     /**
      * Consturctor, whole setup happens here
      */
     protected KeePassApplet() {
-        password = new byte[128];
+        password = new byte[128]; //not set initially.
+        m_ramArray = JCSystem.makeTransientByteArray((short) 128, JCSystem.CLEAR_ON_DESELECT);
+        //init key
+        m_aesKey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+        
+        // create objects for cbc ciphering
+        m_encryptCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+        m_decryptCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
 
-        // INIT GLOBAL PIN
+        // initialize random object
+        m_secureRandom = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
+        
+        // create random AES key, used to encrypt/decrypt the password, no-one except the card knows the key
+        // the only purpose is to keep the password encrypted on the card
+        m_secureRandom.generateData(m_ramArray, (short) 0, (short) 16);
+        m_aesKey.setKey(m_ramArray, (short) 0);
+        
+        //init IV
+        m_secureRandom.generateData(m_ramArray, (short) 0, (short) 16);
+        m_encryptCipher.init(m_aesKey, Cipher.MODE_ENCRYPT, m_ramArray, (short) 0, (short) 16);
+        m_decryptCipher.init(m_aesKey, Cipher.MODE_DECRYPT, m_ramArray, (short) 0, (short) 16);
+        
+        // Initialize PIN to '0000'
         clientPIN = new OwnerPIN((byte) 3, (byte) 4);
         byte[] initial_pin = {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
         clientPIN.update(initial_pin, (short) 0, (byte) 4);
 
+        // register this instance
         register();
     }
 
@@ -153,8 +182,12 @@ public class KeePassApplet extends javacard.framework.Applet {
         short dataLength = apdu.setIncomingAndReceive();
         
         passwordLength = dataLength;
-        Util.arrayCopyNonAtomic(apduBuff, ISO7816.OFFSET_CDATA, password, (byte) 0, dataLength);
-            
+       
+        // encrypt the incoming buffer straight away and store the result. 
+        // notice that we always encrypt 128 bytes. As javacard does not support padding
+        // it is the reasonable way how to handle it. As we encrypt "something" and we keep the length
+        // of the password saved (length is unencrypted), we always know how much to send back.
+        m_encryptCipher.doFinal(apduBuff, ISO7816.OFFSET_CDATA, (short) 128, password, (short) 0);
     }
 
     /**
@@ -165,7 +198,11 @@ public class KeePassApplet extends javacard.framework.Applet {
             ISOException.throwIt(SW_NOT_AUTHENTICATED);
         }
         byte[] apduBuff = apdu.getBuffer();
-        Util.arrayCopyNonAtomic(password, (short) 0, apduBuff, ISO7816.OFFSET_CDATA, passwordLength);
+        
+        //decrypt the password to the outgoing buffer
+        m_decryptCipher.doFinal(password, (short) 0, (short) 128, apduBuff, ISO7816.OFFSET_CDATA);
+        
+        //buffer is filled with decrypted 128bytes, but only "passwordLength" bytes is sent.
         apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, passwordLength);
         
     }
